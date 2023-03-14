5a2593a1e283a65cff81e959dc44831cf2e125f4
Bryce Leung <74439762+Bryce-Leung@users.noreply.github.com>
2023-03-14 14:12:00 -0700

    Add files via upload

---
+++ src/fifteenpuzzle/Solver.java
@@ -0,0 +1,26 @@
+package fifteenpuzzle;
+
+import java.io.File;
+import java.lang.invoke.MethodHandles;
+
+public class Solver {
+	public static void main(String[] args) {
+//		System.out.println("number of arguments: " + args.length);
+//		for (int i = 0; i < args.length; i++) {
+//			System.out.println(args[i]);
+//		}
+
+		if (args.length < 2) {
+			System.out.println("File names are not specified");
+			System.out.println("usage: java " + MethodHandles.lookup().lookupClass().getName() + " input_file output_file");
+			return;
+		}
+		
+		
+		//TODO
+		//File input = new File(args[0]);
+		// solve...
+		//File output = new File(args[1]);
+
+	}
+}
