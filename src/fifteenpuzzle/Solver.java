0445a84fd79be5032a04a931879ebe5002924e5f
Bryce-Leung <jambuttercup1234@gmail.com>
2023-03-21 12:41:01 -0700

    uncommented

--- src/fifteenpuzzle/Solver.java
+++ src/fifteenpuzzle/Solver.java
@@ -5,14 +5,17 @@ import java.lang.invoke.MethodHandles;
 import java.util.*;
 
 public class Solver {
+
+	// Initialize privtate variables
+	private int boardSize = 0;
+
 	public static void main(String[] args) {
-//		System.out.println("number of arguments: " + args.length);
-//		for (int i = 0; i < args.length; i++) {
-//			System.out.println(args[i]);
-//		}
+		System.out.println("number of arguments: " + args.length);
+		for (int i = 0; i < args.length; i++) {
+			System.out.println(args[i]);
+		}
 
-
-
+		// Checks if an argument has been passed in by the user
 		if (args.length < 2) {
 			System.out.println("File names are not specified");
 			System.out.println("usage: java " + MethodHandles.lookup().lookupClass().getName() + " input_file output_file");
