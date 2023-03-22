ca1f8866413e9e6bd87c3ec209a0c99f05b51a4f
Bryce-Leung <jambuttercup1234@gmail.com>
2023-03-21 17:52:08 -0700

    added the basis for the correct solution checker

--- src/fifteenpuzzle/Solver.java
+++ src/fifteenpuzzle/Solver.java
@@ -9,7 +9,21 @@ public class Solver {
 	// Initialize privtate variables
 	private int boardSize = 0;
 
+	private boolean boardCorrect() {
+		// For loop that runs through depending on the size of the puzzle to see if it is correct
+		for(int i = 0; i < (boardSize-1); i++) {
+			//if( != (i+1)) {
+			//	return false;
+			// }
+		}
+		// If no problems are detected during the check
+		return true;
+	}
+
+
 	public static void main(String[] args) {
+
+		// Prints out the argument size provided
 		System.out.println("number of arguments: " + args.length);
 		for (int i = 0; i < args.length; i++) {
 			System.out.println(args[i]);
@@ -22,10 +36,16 @@ public class Solver {
 			return;
 		}
 		
-		
+
 		//TODO
 		//File input = new File(args[0]);
 		// solve...
+
+
+
+		// Check if the solution is correct
+
+
 		//File output = new File(args[1]);
 
 	}
