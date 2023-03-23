5e77a4bf6bd24e8ba08091d0b47b1583958dc52e
Bryce-Leung <jambuttercup1234@gmail.com>
2023-03-22 18:00:50 -0700

    added comments

--- src/fifteenpuzzle/positional.java
+++ src/fifteenpuzzle/positional.java
@@ -1,9 +1,11 @@
 package fifteenpuzzle;
 
 public class positional {
+    // Initialize public variables
     public int x;
     public int y;
 
+    // Constructor that creates the positional object
     public positional(int x, int y) {
         this.x = x;
         this.y = y;
